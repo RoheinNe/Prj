11aff26bab6cc6050c384a9d9ba0d20c14367355
Heather <charlottecaelestis@gmail.com>
2023-05-21 13:42:24 +0800

    UI Search Result, WIP Reserve Vehicle

---
+++ app/src/main/java/com/example/prj/reserve_vehincle.java
@@ -0,0 +1,14 @@
+package com.example.prj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class reserve_vehincle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reserve_vehincle);
+    }
+}
--- app/src/main/java/com/example/prj/search_results.java
+++ app/src/main/java/com/example/prj/search_results.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Featured_Vehicles extends AppCompatActivity {
+public class search_results extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_featured_vehicles);
+        setContentView(R.layout.activity_search_results);
     }
 }
