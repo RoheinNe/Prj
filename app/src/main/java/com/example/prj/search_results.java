d48972dd30c53f49a302207ad8cf2ec8eb7012c3
Jethro Cenas <jethrocenas@gmail.com>
2023-05-21 22:58:41 +0800

    recyclerview Added on search

--- app/src/main/java/com/example/prj/search_results.java
+++ app/src/main/java/com/example/prj/search_results.java
@@ -1,14 +1,44 @@
 package com.example.prj;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 
-public class search_results extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
+public class search_results extends AppCompatActivity implements RecyclerViewInterface {
+    ArrayList<BusModel> busModels = new ArrayList<>();
+    String busName;
+    String busDate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_results);
+
+        Intent intent = getIntent();
+        busName = intent.getStringExtra(search_vehicle.EXTRA_BUS);
+        busDate = intent.getStringExtra(search_vehicle.EXTRA_DATE);
+//        busName = "CEBU";
+//        busDate = "12:30";
+        RecyclerView recyclerView = findViewById(R.id.mRecyclerView);
+        setBusModels();
+
+        BM_RecyclerViewAdapter adapter = new BM_RecyclerViewAdapter(this, busModels, this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    private void setBusModels() {
+        for (int i = 0; i < 10; i++) {
+            busModels.add(new BusModel(busName, busDate));
+        }
+    }
+
+    @Override
+    public void onItemClick(int position) {
+
     }
 }
--- app/src/main/java/com/example/prj/search_vehicle.java
+++ app/src/main/java/com/example/prj/search_vehicle.java
@@ -3,6 +3,9 @@ package com.example.prj;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,14 +13,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class search_vehicle extends AppCompatActivity {
+    public static final String EXTRA_BUS = "com.example.prj.EXTRA_BUS";
+    public static final String EXTRA_DATE = "com.example.prj.EXTRA_DATE";
     BottomNavigationView bottomNavigationView;
+    String from, to, date;
+    Button searchBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_vehicle);
         bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigator);
         bottomNavigationView.setSelectedItemId(R.id.ic_book);
-
+        searchBtn = (Button) findViewById(R.id.searchBtn);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -30,6 +37,20 @@ public class search_vehicle extends AppCompatActivity {
                         return true;
                 }
                 return true;
+            }
+        });
+
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                from = ((EditText) findViewById(R.id.fromTF)).getText().toString();
+                to = ((EditText) findViewById(R.id.toTF)).getText().toString();
+                date = ((EditText) findViewById(R.id.dateTF)).getText().toString();
+
+                Intent intent = new Intent(getApplicationContext(), search_results.class);
+                intent.putExtra(EXTRA_BUS, to);
+                intent.putExtra(EXTRA_DATE, date);
+                startActivity(intent);
             }
         });
     }
